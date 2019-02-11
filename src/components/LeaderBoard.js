e39379856d8c55e434368697a418d7a8a6c92fca
mauricio oporto <fmod@the-dsw.com>
2019-02-11 11:43:30 +0100

    feat: LeaderBoard view

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,13 +1,54 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 
 class LeaderBoard extends Component {
     render() {
+        const { users, placement } = this.props
+
         return (
             <div>
-                Leader Board
+                {placement.sort((a,b) => b.score - a.score ).map(user => {
+                    const totalQuestions = users[user.id].questions.length
+                    const totalAnswers = Object.keys(users[user.id].answers).length
+                    const score = totalQuestions + totalAnswers
+
+                    return (
+                        <div className="leaderboard-container" key={user.id}>
+                            <img
+                                src={require(`../images/${users[user.id].avatarURL}.jpg`)}
+                                alt={`Avatar of ${users[user.id].avatarURL}`}
+                                className="avatar-question"
+                            />
+                            <div className="vl"></div>
+                            <div className="leaderboard-info">
+                                <span className="info-title">{users[user.id].name}</span>
+                                <span className="info-answered"> Answered Questions <span>{totalAnswers}</span></span>
+                                <div className="hl-info"></div>
+                                <span className="info-created"> Created Questions <span>{totalQuestions}</span></span>
+                            </div>
+                            <div className="vl"></div>
+                            <div className="leaderboard-score">
+                                <div className="score-title">score</div>
+                                <div className="score-points">{score}</div>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
 }
 
-export default LeaderBoard
+function mapStateToPorps ({ users }) {
+    const userIds = Object.keys(users)
+    const placement = userIds.map(id => ({
+        id,
+        score: Object.keys(users[id].answers).length + Object.keys(users[id].questions).length
+    }))
+
+    return {
+        users,
+        placement
+    }
+}
+export default connect(mapStateToPorps)(LeaderBoard)
